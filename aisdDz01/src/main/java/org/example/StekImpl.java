a2ea4e41437e2a88973d6b2c6aa6b653bb7e07ca
kibersashka <aleksandraveldaeva4@gmail.com>
2025-04-27 14:06:55 +0300

    create interface

---
+++ aisdDz01/src/main/java/org/example/StekImpl.java
@@ -0,0 +1,39 @@
+package org.example;
+//<T> - переменная типа данных
+public interface StekImpl<T> {    /**
+ * метод добавляет элемент в список *в конец*
+ *
+ */
+void add(T x);
+    /**
+     * метод добавляет элемент в позицию i
+     */
+    void add(T x, int i) throws IndexOutOfBoundsException;
+    /**
+     *
+     *
+     * удаление оп индексу i и возвращение удаленного элемента
+     */
+    void remove();
+    T get();
+    /**
+     * длина списка
+     */
+    void clear();
+
+    int size();
+    /**
+     * обращение по индексу
+     * Integer --->>> null
+     */
+    T get(int i) throws IndexOutOfBoundsException;
+
+    /**
+     *
+     * sortirovka
+     * acs - направление сортировки
+     * true - возрастание
+     */
+
+
+}
